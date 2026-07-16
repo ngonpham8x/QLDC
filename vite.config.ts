2af5552fe71042e0b7871922bd7c2a1d45044b5e
ngonpham8x <tayninhdoimoi@gmail.com>
2026-07-16 22:23:50 +0700

    Remove PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
+
 import path from 'path'
 
 export default defineConfig({
@@ -9,40 +9,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
 
-    VitePWA({
-  registerType: 'autoUpdate',
-
-  workbox: {
-    maximumFileSizeToCacheInBytes: 5 * 1024 * 1024
-  },
-
-  includeAssets: [
-    'favicon.ico',
-    'apple-touch-icon.png',
-    'logo-192.png',
-    'logo-512.png'
-  ],
-      manifest: {
-        name: 'Hệ thống Quản lý Dân cư',
-        short_name: 'QLDC',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#0b7a43',
-        icons: [
-          {
-            src: '/logo-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/logo-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+    
   ],
 
   resolve: {
