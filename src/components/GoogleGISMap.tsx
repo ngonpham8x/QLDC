f0617bd60eedde5907ec62e20c851e1e36d897fc
ngonpham8x <tayninhdoimoi@gmail.com>
2026-07-24 15:28:25 +0700

    Fix syntax error in GoogleGISMap

--- src/components/GoogleGISMap.tsx
+++ src/components/GoogleGISMap.tsx
@@ -21,8 +21,8 @@ interface Props {
 const markerIcon = new L.Icon({
     iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
     shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
-    iconSize:,
-    iconAnchor:,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
     popupAnchor: [1, -34],
     shadowSize: [41, 41]
 });
@@ -39,76 +39,83 @@ export default function GoogleGISMap({
 }: Props) {
 
     const gpsHouses = households.filter(
-        h => h.gpsLat !== undefined && h.gpsLng !== undefined
-    );
+    h => h.gpsLat !== undefined && h.gpsLng !== undefined
+);
 
-    const selectedHouse = households.find(
-        h => h.id === selectedHouseId
-    );
+const selectedHouse = households.find(
+    h => h.id === selectedHouseId
+);
 
-    const center: [number, number] =
-        selectedHouse?.gpsLat !== undefined &&
-        selectedHouse?.gpsLng !== undefined
-            ? [
-                  selectedHouse.gpsLat,
-                  selectedHouse.gpsLng
-              ]
-            : gpsHouses.length > 0
-            ? [
-                  gpsHouses[0].gpsLat!,
-                  gpsHouses[0].gpsLng!
-              ]
-            : [11.33871, 106.11864];
+const center: [number, number] =
+    selectedHouse?.gpsLat !== undefined &&
+    selectedHouse?.gpsLng !== undefined
+        ? [
+              selectedHouse.gpsLat,
+              selectedHouse.gpsLng
+          ]
+        : gpsHouses.length > 0
+        ? [
+              gpsHouses[0].gpsLat!,
+              gpsHouses[0].gpsLng!
+          ]
+        : [11.33871, 106.11864];
     
     return (
-        <MapContainer
-            center={center}
-            zoom={16}
-            style={{
-                width: "100%",
-                height: "100%",
-                borderRadius: "12px"
-            }}
-        >
-            <TileLayer
-                attribution="&copy; Esri"
-                url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-            />
 
-            {gpsHouses.map((h) => (
-                <Marker
-                    key={h.id}
-                    icon={markerIcon}
-                    position={[h.gpsLat!, h.gpsLng!]}
-                    eventHandlers={{
-                        click: () => onSelectHouse?.(h.id)
-                    }}
-                >
-                    <Popup>
-                        <strong>{h.id}</strong>
-                        <br />
-                        {h.ownerName}
-                        <br />
-                        {h.address}
-                    </Popup>
-                </Marker>
-            ))}
+    <MapContainer
+        center={center}
+        zoom={16}
+        style={{
+            width: "100%",
+            height: "100%",
+            borderRadius: "12px"
+        }}
+    >
 
-            {selectedHouse && selectedHouse.gpsLat !== undefined && selectedHouse.gpsLng !== undefined && (
-                <Marker
-                    icon={markerIcon}
-                    position={[
-                        selectedHouse.gpsLat,
-                        selectedHouse.gpsLng
-                    ]}
-                >
-                    <Popup>
-                        <b>{selectedHouse.ownerName}</b>
-                        <br />
-                        {selectedHouse.address}
-                    </Popup>
-                </Marker>
-            )}
+        <TileLayer
+    attribution="&copy; Esri"
+    url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+/>
+
+{gpsHouses.map((h) => (
+
+    <Marker
+        key={h.id}
+        icon={markerIcon}
+        position={[h.gpsLat!, h.gpsLng!]}
+        eventHandlers={{
+            click: () => onSelectHouse?.(h.id)
+        }}
+    >
+    <Popup>
+        <strong>{h.id}</strong>
+        <br />
+        {h.ownerName}
+        <br />
+        {h.address}
+    </Popup>
+</Marker>
+
+                )
+
+            })}
+
+            {selectedHouse && (
+    <Marker
+        icon={markerIcon}
+        position={[
+            selectedHouse.gpsLat!,
+            selectedHouse.gpsLng!
+        ]}
+    >
+        <Popup>
+            <b>{selectedHouse.ownerName}</b>
+            <br />
+            {selectedHouse.address}
+        </Popup>
+    </Marker>
+)}
         </MapContainer>
     );
+
 }
